43f1972f5bbc9da53e9d530c465fcf15e0491e34
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-07-31 12:02:28 +0530

    Create deploy.js

---
+++ scipts/deploy.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const CandidateNames = ["A", "B", "C","D", "E", "F"];
+
+  const VotingSystem = await ethers.getContractFactory("VotingSystem");
+  const votingSystem = await VotingSystem.deploy(CandidateNames);
+  await votingSystem.deployed();
+
+  console.log("VotingSystem deployed to:", votingSystem.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
