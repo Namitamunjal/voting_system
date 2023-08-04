d2972d817584dfd964b73b8287203f0771be1d0f
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-08-04 17:56:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,96 @@
 const { ethers } = require("ethers");
-const fs = require("fs");
 
-async function main() {
-  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-  const signer = provider.getSigner();
-
-  const contractAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; 
-  const contractABI = JSON.parse(fs.readFileSync("artifacts/contracts/Assessment.sol/VotingSystem.json")).abi;
-  const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
-  // Read the candidate names from the smart contract
-  const candidatesCount = await contract.candidatesCount();
-  const candidateNames = [];
-  for (let i = 1; i <= candidatesCount; i++) {
-    const candidate = await contract.candidates(i);
-    candidateNames.push(candidate.name);
-  }
-
-  // Display the candidate names on the web page
-  const candidatesList = document.getElementById("candidatesList");
-  candidateNames.forEach((name) => {
-    const candidateItem = document.createElement("li");
-    candidateItem.textContent = name;
-    candidatesList.appendChild(candidateItem);
-  });
-
-  // Voting function
-  const voteForm = document.getElementById("voteForm");
-  voteForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const candidateId = parseInt(document.querySelector('input[name="candidate"]:checked').value, 10);
-    await contract.vote(candidateId);
-    alert("Vote successful!");
-    window.location.reload();
-  });
+async function displayVotes() { 
+  const contractAddress = "0x929AdF89436817637b10cD85AAF533dBE2a8B830"; 
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+   const contract = new ethers.Contract(contractAddress, abi, provider);
+    const [deployer] = await provider.listAccounts(); 
+    // Get the number of candidates from the contract 
+    const candidatesCount = await contract.getCandidatesCount(); 
+    const candidatesDiv = document.getElementById("candidates"); 
+    candidatesDiv.innerHTML = ""; 
+    // Loop through each candidate and display their name and vote count
+    for (let i = 0; i < candidatesCount; i++) 
+    {
+       const name = await contract.getCandidateName(i); 
+       const votes = await contract.getVotesCount(i); 
+       const candidateDiv = document.createElement("div");
+        candidateDiv.classList.add("candidate");
+        const nameHeading = document.createElement("h2"); 
+        nameHeading.textContent = name; candidateDiv.appendChild(nameHeading);
+         const votesParagraph = document.createElement("p");
+          votesParagraph.textContent = "Votes: " + votes.toString();
+           candidateDiv.appendChild(votesParagraph); candidatesDiv.appendChild(candidateDiv); 
+    }
 }
 
-main().catch((error) => {
-  console.error(error);
-});
+async function getVoterCount() {
+  const contractAddress = '0x929AdF89436817637b10cD85AAF533dBE2a8B830';
+  const abi = [
+    // Function to get the total number of voters
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "totalVoters",
+      "outputs": [{"name": "", "type": "uint256"}],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    // Function to get the number of candidates
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getCandidatesCount",
+      "outputs": [{"name": "", "type": "uint256"}],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    // Function to get the name of a candidate by index
+    {
+      "constant": true,
+      "inputs": [{"name": "index", "type": "uint256"}],
+      "name": "getCandidateName",
+      "outputs": [{"name": "", "type": "string"}],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    // Function to get the number of votes received by a candidate
+    {
+      "constant": true,
+      "inputs": [{"name": "candidateIndex", "type": "uint256"}],
+      "name": "getVotesCount",
+      "outputs": [{"name": "", "type": "uint256"}],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+  
+
+  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+
+  try {
+    const voterCount = await contract.totalVoters();
+    document.getElementById('voterCount').textContent = voterCount.toString();
+  } catch (error) {
+    console.error(error);
+    document.getElementById('voterCount').textContent = 'Error loading voter count';
+  }
+}
+
+async function main() {
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+
+    // Call both functions
+    await displayVotes();
+    await getVoterCount();
+  } else {
+    alert("Please install MetaMask to use this application.");
+  }
+}
+main();
